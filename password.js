3729c30d95329c0892f1beebe023fd7bac1da3b5
JHabte <j.habtema@gmail.com>
2020-02-18 01:18:55 -0500

    refactored code so that function waits for click event before running

--- password.js
+++ password.js
@@ -4,26 +4,30 @@ var numArr = "0123456789";
 var specArr = "#$%&()*+-/:;<=>?@[^_{|}~";
 var password = "";
 var passwordBank = "";
-var upperChar = false;
-var lowerChar = false;
-var specialChar = false;
-var numChar = false;
+
 var passwordBox = document.getElementById("password");
 var genButton = document.querySelector('#generate');
 
-var passLength = prompt("Enter your password length");
 
-while (upperChar === false && lowerChar === false && numChar === false && specialChar === false) {
-    var upperChar = confirm("Would you like uppercase letters?");
-    var lowerChar = confirm("Would you like lowercase letters?");
-    var specialChar = confirm("Would you like special characters?");
-    var numChar = confirm("Would you like numbers?");
-}
-while (parseInt(passLength) < 8 || parseInt(passLength) > 128) {
-    var passLength = prompt("Please select a password length between 8 - 128 characters");
-}
 
 function generatePassword() {
+    var upperChar = false;
+    var lowerChar = false;
+    var specialChar = false;
+    var numChar = false;
+    // passwordBox.value = "";
+    var passLength = prompt("Enter your password length");
+    console.log("test");
+    while (upperChar === false && lowerChar === false && numChar === false && specialChar === false) {
+
+        var upperChar = confirm("Would you like uppercase letters?");
+        var lowerChar = confirm("Would you like lowercase letters?");
+        var specialChar = confirm("Would you like special characters?");
+        var numChar = confirm("Would you like numbers?");
+    }
+    while (parseInt(passLength) < 8 || parseInt(passLength) > 128) {
+        var passLength = prompt("Please select a password length between 8 - 128 characters");
+    }
     if (upperChar === true) {
         passwordBank += upperArr;
     }
@@ -40,7 +44,14 @@ function generatePassword() {
     for (i = 0; i < passLength; i++) {
         password = password + passwordBank.charAt(Math.floor((Math.random() * passwordBank.length) + 1));
     }
-    passwordBox.textContent = password;
-}
 
-genButton.addEventListener('click', generatePassword);
+
+    passwordBox.value = password;
+}
+genButton.onclick = function() {
+    passwordBox.value = "";
+    generatePassword();
+};
+
+
+// genButton.addEventListener('click', generatePassword);
